bef510758a0f0568c8708ebdc1bdb2b7571c3f62
blessedmane <92204851+blessedmane@users.noreply.github.com>
2022-02-08 11:09:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,324 @@
+// zadacha 1
+// let num = 3
+// alert(num)
+
+// zadacha 2
+// let a = 10
+// let b = 2
+// alert(a+b)
+// alert(a-b)
+// alert(a*b)
+// alert(a/b)
+
+// zadacha 3
+// let c = 15
+// let d = 2
+// let result = c+d
+// alert(result)
+
+// zadacha 4
+// let a = 10
+// let b = 2
+// let c = 5
+// alert(a+b+c)
+
+// zadacha 5
+// let a = 17
+// let b = 10
+// let c = a-b
+// let d = 7
+// let result = c+d
+// alert(result)
+
+// zadacha 6
+// let str = `Привет, мир!`
+// alert(str)
+
+// zadacha 7
+// let str1 = `Привет, `
+// let str2 = `мир!`
+// alert(str1+str2)
+
+// zadacha 8
+// let name = `Bodyan`
+// alert(`Привет, ${name}`)
+
+// zadacha 9
+// let age = 15
+// alert(`Тебе ${age} лет`)
+
+// zadacha 10
+// let name = prompt(`Vvedi svoe imya`, `Bodya`)
+// alert(`Tvoe imya ${name}`)
+
+// zadacha 11
+// let num = prompt(`Vvedi chislo`)
+// alert(num**2)
+
+// zadacha 12
+// let str = `abcde`
+// alert(str[0])
+// alert(str[2])
+// alert(str[4])
+
+// zadacha 13
+// let num = `12345`
+// alert(num[0]*num[0])
+// alert(num[1]*num[1])
+// alert(num[2]*num[2])
+// alert(num[3]*num[3])
+// alert(num[4]*num[4])
+
+// zadacha 14
+// console.log(`В часе ${60*60} секунд`)
+// console.log(`В сутках ${60*60*24} секунд`)
+// console.log(`В месяце ${60*60*24*30} секунд`)
+
+// zadacha 15
+// let newDate = new Date()
+// let hours = newDate.getHours()
+// let minutes = newDate.getMinutes()
+// let seconds = newDate.getSeconds()
+// console.log(`${hours}:${minutes}:${seconds}`)
+
+// zadacha 16
+// let num = prompt(`Vvedi chislo`)
+// console.log(num**2)
+
+// zadacha 17
+// let num = 47
+// num+= 7
+// num-= 18
+// num*= 10
+// num/= 15
+// console.log(num);
+
+// zadacha 18
+// let num = 10
+// num++
+// num++
+// num--
+// alert(num)
+
+// zadacha 1.1
+// let a = prompt(`Vvedi chislo`)
+// if (a==0){
+//     console.log(`Verno!`)
+// }
+// else{
+//     console.log(`Neverno!`)
+// }
+
+// zadacha 1.2
+// let a = prompt(`Vvedi chislo`)
+// if (a>0){
+//     console.log(`Verno!`)
+// }
+// else{
+//     console.log(`Neverno!`)
+// }
+
+// zadacha 1.3
+// let a = prompt(`Vvedi chislo`)
+// if (a<0){
+//     console.log(`Verno!`)
+// }
+// else{
+//     console.log(`Neverno!`)
+// }
+
+// zadacha 1.4
+// let a = prompt(`Vvedi chislo`)
+// if (a>=0){
+//     console.log(`Verno!`)
+// }
+// else{
+//     console.log(`Neverno!`)
+// }
+
+// zadacha 1.5
+// let a = prompt(`Vvedi chislo`)
+// if (a<=0){
+//     console.log(`Verno!`)
+// }
+// else{
+//     console.log(`Neverno!`)
+// }
+
+// zadacha 1.6
+// let a = prompt(`Vvedi chislo`)
+// if (a!=0){
+//     console.log(`Verno!`)
+// }
+// else{
+//     console.log(`Neverno!`)
+// }
+
+// zadacha 1.7
+// let a = prompt(`Vvedi text`)
+// if (a==`test`){
+//     console.log(`Verno!`)
+// }
+// else{
+//     console.log(`Neverno!`)
+// }
+
+// zadacha 1.8
+// let a = prompt(`Vvedi chislo`)
+// if (a===`1`){
+//     console.log(`Verno!`)
+// }
+// else{
+//     console.log(`Neverno!`)
+// }
+
+// zadacha 1.9
+// let a = confirm(`Choose`)
+// if (a==true){
+//     console.log(`Verno!`)
+// }
+// else{
+//     console.log(`Neverno!`)
+// }
+
+// let a = confirm(`Choose`)
+// if (a==true){
+//     console.log(`Verno!`)
+// }
+// if (a==false){
+//     console.log(`Neverno!`)
+// }
+
+// zadacha 1.10
+// let a = confirm(`Choose`)
+// if (a!==true){
+//     console.log(`Verno!`)
+// }
+// else{
+//     console.log(`Neverno!`)
+// }
+
+// let a = confirm(`Choose`)
+// if (a!==true){
+//     console.log(`Verno!`)
+// }
+// if (a==true){
+//     console.log(`Neverno!`)
+// }
+
+// zadacha 1.11
+// let a = prompt(`Vvedi chislo`)
+// if (a>0 && a<5){
+//     console.log(`Verno!`)
+// }
+// else{
+//     console.log(`Neverno!`)
+// }
+
+// zadacha 1.12
+// let a = Number(prompt(`Vvedi chislo`))
+// if (a==0 || a==2){
+//     console.log(a+7)
+// }
+// else{
+//     console.log(a/10)
+// }
+
+// zadacha 1.13
+// let a = Number(prompt(`Vvedi chislo`))
+// let b = Number(prompt(`Vvedi chislo`))
+// if (a<=1 && b>=3){
+//     console.log(a+b)
+// }
+// else{
+//     console.log(a/b)
+// }
+
+// zadacha 1.14
+// let a = Number(prompt(`Vvedi chislo`))
+// let b = Number(prompt(`Vvedi chislo`))
+// if (a>=2 && a<=11 || b>=6 && b<14){
+//     console.log(`Verno!`)
+// }
+// else{
+//     console.log(`Neverno`)
+// }
+
+// zadacha 1.15
+// let num = Number(prompt(`Vvedi chislo 1-4`))
+// switch (num){
+//     case 1:
+//         console.log(`Зима`);
+//         break;
+//     case 2:
+//         console.log(`Весна`);
+//         break;
+//     case 3:
+//         console.log(`Лето`);
+//         break;
+//     case 4:
+//         console.log(`Осень`);
+//         break;
+//     default:
+//         console.log(`Введи правильное число!`);
+// }
+
+// zadacha 1.16
+// let day = Number(prompt(`Vvedi den'`))
+// if (day >= 1 && day <= 14) {
+// 	alert('В 1');
+// }
+// if (day >= 15 && day <= 28) {
+// 	alert('Во 2');
+// }
+// if (day >= 29 && day <= 31) {
+// 	alert('В 3');
+// }
+
+// zadacha 1.17
+// let month = Number(prompt(`Vvedi mesyac`))
+// if (month == 12 || month == 1 || month == 2) {
+// 	alert('Зима');
+// }
+// if (month == 3 || month == 4 || month == 5) {
+// 	alert('Весна');
+// }
+// if (month == 6 || month == 7 || month == 8) {
+// 	alert('Лето');
+// }
+// if (month == 9 || month == 10 || month == 11) {
+// 	alert('Осень');
+// }
+
+// zadacha 1.18
+// let str = `abcde`
+// if (str[0] === `a`){
+//     console.log(`da`)
+// }
+// else{
+//     console.log(`net`)
+// }
+
+// zadacha 1.19
+// let num = `12345`
+// if (num[0] == 1 || num[1] == 2 || num[2] == 3){
+//     console.log(`da`)
+// }
+// else{
+//     console.log(`net`)
+// }
+
+// zadacha 1.20
+// let num = `123`
+// console.log(Number(num[0])+Number(num[1])+Number(num[2]))
+
+// zadacha 1.21
+// let num = `123456`
+// let result1 = Number(num[0]+Number(num[1])+Number(num[2]))
+// let result2 = Number(num[3]+Number(num[4])+Number(num[5]))
+// if (result1 == result2){
+//     console.log(`da`)
+// }
+// else{
+//     console.log(`net`)
+// }
